bd6269aa4ff808d841a7dfe355f144dfc2aa0d76
assassin871 <saijvikneshan@gmail.com>
2024-12-18 01:12:12 +0530

    added participant class

---
+++ Participant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS
+{
+    internal class Participant
+    {
+        public required string name { get; set; }
+        public required string email { get; set; }
+    }
+}
